59a705e485ae9bce986e6aaefebfff244f604dbc
thaseus <thaseus@uw.edu>
2022-02-01 14:08:14 -0800

    V2

--- Trafficmodel.py
+++ Trafficmodel.py
@@ -1,55 +1,41 @@
 # Imports
 from numpy import array, linspace
-from matplotlib.pyplot import figure, subplot, plot
+from matplotlib.pyplot import figure, subplot, show
 from scipy.integrate import odeint
 
 # Initial Variables (In mks units)
-a = 0.7 # Acceleration
-b = 2. # Deceleration
-delta = 4. # Acceleration exponent
-L = 5. # Vehicle length
-xmin = 2. # Minimum gap
-T = 1.8 # Time headway
-vdes = 28 # Desired speed
-Xblock = 5000 # Barrier position
+a = 0.3  # Acceleration
+b = 2.  # Deceleration
+delta = 4.  # Acceleration exponent
+L = 5.  # Vehicle length
+xmin = 2.  # Minimum gap
+T = 1.8  # Time headway
+vdes = 28  # Desired speed
+Xblock = 5000  # Barrier position
 
 # Initial Conditions
 x0 = 0
 v0 = 0
-V = array([x0,v0])
+V1 = array([x0, v0])
 
 # set the time interval for solving
 t0 = 0
-tf = Xblock / vdes
+tf = 100
 
 # Form Time array
-t = linspace(t0,tf,400) # 400 steps for nice plot
+tspace = linspace(t0, tf, 400)  # 400 steps for nice plot
+
 
 def main():
-    X = odeint(singleratefunc, V, t, tfirst=True)
+    X = odeint(singleratefunc, V1, tspace, tfirst=True)
 
     # unpack the results. In the output array, variables are columns, times are rows
     xout = X[:, 0]
-    tout = X[:, 1]
-
+    vout = X[:, 1]
 
     # For plotting
-    fig = figure()
+    plot(xout, vout)
 
-    ax1 = subplot()
-    ax1.plot(t, xout, 'b')
-    ax1.set_xlabel('time (s)')
-    ax1.set_ylabel('distance (m)', color='b')
-    ax1.tick_params('y', colors='b')
-
-    ax2 = ax1.twinx()
-    ax2.plot(t, tout, 'r')
-    ax2.set_ylabel('velocity (m/s)', color='r')
-    ax2.tick_params('y', colors='r')
-
-    fig.tight_layout()
-
-    plot()
 
 # Differential Equation
 def singleratefunc(t, V):
@@ -62,7 +48,7 @@ def singleratefunc(t, V):
 
     # Compute acceleration from IDM
 
-    a_idm = a * (1 - (v / vdes)**delta)
+    a_idm = a * (1 - (v / vdes) ** delta)
 
     # compute derivatives
     dx = v
@@ -73,7 +59,24 @@ def singleratefunc(t, V):
     return rate
 
 
+# Function used to plot everything
+def plot(x1, x2):
+    fig = figure()
+
+    ax1 = subplot()
+    ax1.plot(tspace, x1, 'b')
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('distance (m)', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(tspace, x2, 'r')
+    ax2.set_ylabel('velocity (m/s)', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+
+    show()
+
 
 main()
-
-
